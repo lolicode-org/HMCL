2cd82111ce2c6a0a751387371dd21880232f6094
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2025-10-13 16:54:14 +0800

    drop integrity check

--- HMCL/src/main/java/org/jackhuang/hmcl/upgrade/IntegrityChecker.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/upgrade/IntegrityChecker.java
@@ -45,7 +45,7 @@ import static org.jackhuang.hmcl.util.logging.Logger.LOG;
 public final class IntegrityChecker {
     private IntegrityChecker() {}
 
-    public static final boolean DISABLE_SELF_INTEGRITY_CHECK = "true".equals(System.getProperty("hmcl.self_integrity_check.disable"));
+    public static final boolean DISABLE_SELF_INTEGRITY_CHECK = true;
 
     private static final String SIGNATURE_FILE = "META-INF/hmcl_signature";
     private static final String PUBLIC_KEY_FILE = "assets/hmcl_signature_publickey.der";
